4a43d0a093d09e9845c31281864ab93c68b26a74
asolano33 <solanoa97@gmail.com>
2025-08-17 18:02:10 +0000

    Dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let container = document.querySelector("#domain-list");
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        let domain = `${p}${a}${n}.com`;
+        console.log(domain); 
+        let li = document.createElement("li");
+        li.textContent = domain;
+        container.appendChild(li);
+      }
+    }
+  }
 };
